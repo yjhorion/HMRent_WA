48c692982fa7234a7d63cb3f7ac0caa39e277ba8
JHyun Y <yjhorion@gmail.com>
2024-04-25 14:10:19 +0900

    데이터 유실 처리 완료

--- src/app.js
+++ src/app.js
@@ -4,10 +4,77 @@ const morgan = require('morgan');
 const path = require('path');
 const AWS = require('aws-sdk');
 const multerS3 = require('multer-s3');
-
 require('dotenv').config();
 
-const {S3ACCESS, S3SECRET, S3BUCKETNAME, SQSQUEUEURL} = process.env;
+// router = express.Router()
+
+const app = express();
+const PORT = 3000;
+
+const { S3ACCESS, S3SECRET, S3BUCKETNAME } = process.env;
+
+
+app.use(morgan('combined'));
+
+const s3 = new AWS.S3({
+    accessKeyId: S3ACCESS,
+    secretAccessKey: S3SECRET,
+    region: 'us-east-1'
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: S3BUCKETNAME,
+        acl: 'public-read',
+        limits: { fileSize: 5 * 1024 * 1024},
+        key: function(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + '-' + Date.now() +ext)
+        }
+    })
+});
+
+app.get('/', (req,res) => {
+    
+    res.sendFile('index.html', { root: __dirname });
+});
+
+
+// 이미지 업로드를 라우트
+app.post('/upload', upload.array('images', 50), (req, res) => {
+
+    console.log('업로드된 파일 정보:', req.files);
+    
+
+    const imageURLs = req.files.map(file => file.location);
+    
+    res.json({ imageURLs });
+});
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+    console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+/** 
+require('dotenv').config();
+
+const {S3ACCESS, S3SECRET, S3BUCKETNAME, //SQSQUEUEURL
+} = process.env;
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +87,13 @@ const s3 = new AWS.S3({
     secretAccessKey: S3SECRET,
     region: "us-east-1" 
 });
+
+// 랜딩페이지
+app.get('/', (req,res) => {
+    
+    res.sendFile('index.html', { root: __dirname });
+});
+
 
 const upload = multer({
     storage: multerS3({
@@ -36,45 +110,43 @@ const upload = multer({
     })
 })
 
-const sqs = new AWS.SQS({
-    accessKeyId: S3ACCESS,
-    secretAccessKey: S3SECRET,
-    region: "us-east-1"
-})
 
-app.get('/', (req,res) => {
-    res.sendFile('index.html', { root: __dirname });
+
+// upload 버튼을 누를 경우 작동하는 post
+app.post('/upload', upload.array('images', 50), async (req, res) => {
+    const uploadedFiles = req.files;
+    const totalFiles = uploadedFiles.length;
+
+    try {
+        console.log(`${totalFiles}개의 파일 업로드 시작됨`);
+
+        for (let index = 0; index < totalFiles; index++) {
+            const file = uploadedFiles[index];
+            console.log(`${index + 1}번 파일 : ${file.originalname}`);
+
+            const uploadResult = await s3.upload({
+                Bucket: S3BUCKETNAME,
+                Key: Date.now().toString() + path.extname(file.originalname),
+                Body: file.buffer,
+                ACL: 'public-read'
+            }).promise();
+
+            console.log(`파일 업로드 성공 (${file.originalname}):`, uploadResult.Location);
+        }
+
+        res.send('업로드 완료');
+    } catch (error) {
+        console.error('에러 :', error);
+        return res.status(500).send('업로드 에러');
+    }
 });
 
-app.post('/upload', upload.array('images', 50), (req, res) => {
-    const uploadedFiles = req.files;
-    const totalFiles = req.files.length;
 
-    console.log(`${totalFiles}개의 파일 업로드됨`);
-    
-    uploadedFiles.forEach((file, index) => {
-        console.log(`${index + 1}번 파일 : ${file.originalname} : 업로드 성공`);
-    });
 
-    // 이미지 업로드 요청을 SQS에 보냄
-    const params = {
-        MessageBody: 'Image uploaded',
-        QueueUrl: SQSQUEUEURL,
-        MessageGroupId: 'S3UploadTest'
-    };
-
-    sqs.sendMessage(params, (err, data) => {
-        if (err) {
-            console.error('SQS 전송 오류:', err);
-        } else {
-            console.log('SQS 전송 성공:', data.MessageId);
-        }
-    });
-
-    res.send('업로드 완료')
-})
 
 
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
+
+**/
