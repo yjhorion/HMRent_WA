a02f9053ca3bd1311540a3f116b501a53c40e82e
Jhyun Y <yjhorion@gmail.com>
2024-06-09 13:27:45 +0900

    .

--- src/routes/compQC.js
+++ src/routes/compQC.js
@@ -175,12 +175,12 @@ router.get('/CompQC/:STATUSREQ', async (req, res, next) => {
                       ]
 
         decryptedresponse = decrypt(response.data, secret_key, IV);
-        console.log("Response received:", response.data, reqCode);
+        console.log("Response received:", response.data);
         console.log("복호화 된 응답값 :", decryptedresponse);
 
         /* 프론트에 데이터를 보내는 부분. stringify 되었던 데이터를 parse 해서 json형식으로 보내줌 */
         res.send({
-            data: JSON.parse(decryptedresponse),
+            data: JSON.parse(decryptedresponse, reqCode),
         })
 
     } catch (error) {
