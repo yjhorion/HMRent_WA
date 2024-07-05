d310dd4e35546d8147169eacc52f40e708800e92
JHyun Y <yjhorion@gmail.com>
2024-07-05 10:45:08 +0900

    이미지 크기 10mb로 상향

--- src/routes/compQC.js
+++ src/routes/compQC.js
@@ -46,7 +46,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
     storage : storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 파일 크기 제한
+    limits: { fileSize: 10 * 1024 * 1024 }, // 파일 크기 제한
     fileFilter: (req, file, cb) => {
         if (!file.mimetype.startsWith('image/')) {
             return cb(new Error('이미지 파일만 업로드 가능합니다.'));
